e73bc7464c76b363fd05b8c6eae67f58758ed765
Alexa Jakob <alexajakob@tutanota.com>
2021-07-19 11:30:12 -0400

    add copyright notices to all code

--- validation/i2c_func.sh
+++ validation/i2c_func.sh
@@ -2,13 +2,31 @@
 
 # I2C TESTING SCRIPT
 
-# By Alexa Jakob for the educational baseboard
+# Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
 
-# assuming we have i2c chips to flash, this script tests i2c functionality by writing and reading
+
+# This script tests i2c functionality by writing and reading
 # to a chip 500 times. It also checks that the clock does not exceed the maximum speed.
 
 # install i2c-tools, if necessary
-#sudo apt install i2c-tools
+# sudo apt install i2c-tools
 
 # detect i2c devices
 echo -e "Please type the bus you wish to check:"
